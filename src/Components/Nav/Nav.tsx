ce8bc6b9abc5451bb66b96f012ae8b2566054140
bananabrann <plbrannan95@gmail.com>
2021-03-27 19:27:36 -0400

    Nav bar styling

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -3,12 +3,17 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.scss";
 
-const Nav: React.FC<any> = (props: any) => { 
+const Nav: React.FC<any> = () => {
     return (
         <div className="Navbar">
             <div className="nav">
-                <p>About Me</p>
-                <p>Freelance Services</p>
+                <Link to="/about" className="nav-about">
+                    <p>About Me</p>
+                </Link>
+
+                <Link to="/freelance" className="nav-freelance">
+                    <p>Freelance Services</p>
+                </Link>
             </div>
         </div>
     );
