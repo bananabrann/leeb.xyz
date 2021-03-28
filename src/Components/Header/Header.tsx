d69c5b61941b3da9ad7f3d205e702b7b4c7b39c6
bananabrann <plbrannan95@gmail.com>
2021-03-28 11:36:30 -0400

    Header with tilt background

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.scss";
+
+const Header: React.FC<any> = () => {
+    return (
+        <div className="Header">
+            <div className="header-content">
+                <p>Hello</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
