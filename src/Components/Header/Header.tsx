1bee762173d45b068c29bc43675cabdf3f2b2ac3
bananabrann <plbrannan95@gmail.com>
2021-03-28 11:50:52 -0400

    Header styling and new logo component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
+import Logo from "../Logo/Logo";
 import "./Header.scss";
 
 const Header: React.FC<any> = () => {
+
     return (
         <div className="Header">
             <div className="header-content">
+                <Logo />
                 <p>Hello</p>
             </div>
         </div>
