b5109eafa13ea7cebf95b53823838b7be5d40a98
bananabrann <plbrannan95@gmail.com>
2021-03-28 12:15:15 -0400

    Add NavItem components

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { useEffect } from "react";
 import Logo from "../Logo/Logo";
+import NavItem from "./NavItem/NavItem";
 import "./Header.scss";
 
 const Header: React.FC<any> = () => {
-
     return (
         <div className="Header">
             <div className="header-content">
                 <Logo />
-                <p>Hello</p>
+
+                <NavItem
+                    destination="/about"
+                    label="About Me"
+                    isDisabled={false}
+                    navItemIdentifier="navitem-about"
+                />
+
+                <NavItem 
+                    destination="/freelance"
+                    label="Freelance Services"
+                    isDisabled={false}
+                    navItemIdentifier="navitem-freelance"
+                />
             </div>
         </div>
     );
