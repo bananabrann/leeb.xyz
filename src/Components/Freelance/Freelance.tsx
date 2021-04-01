b8588081133f65a6dd51f6d323c5657d24ad9d24
bananabrann <plbrannan95@gmail.com>
2021-04-01 13:01:21 -0400

    Wording changes

--- src/Components/Freelance/Freelance.tsx
+++ src/Components/Freelance/Freelance.tsx
@@ -14,14 +14,14 @@ const Freelance: React.FC<any> = () => {
             <div className="wide-content content-wrap">
                 <FaqItem
                     question="What technologies do you use?"
-                    answer="I have experience in everything from React and TypeScript to C# and Java. However, it doesn't matter. I've learned through my experience is that it doesn't really matter what I already know --what tech stack we choose to use entirely depends on your project and needs."
+                    answer="I have experience in a lot of things ranging from React and TypeScript to Python and Java. However, I've learned through my experience that it doesn't really matter. What tech stack we choose is entirely dependent on your project and needs."
                 />
 
                 <FaqItem
                     question="How much do you charge?"
-                    answer="I do not charge hourly; I charge by the project. I cannot give you an honest estimate to a solution without understanding your problem. The price is drastically effected by what your needs are. A business user looking to automate their Excel worksheets might be $100, while a complex, enterprise eCommerce website might be several thousand. Let's talk about what your specific needs are."
+                    answer="I do not charge hourly; I charge by the project. I cannot give you an honest estimate to a solution without understanding your problem. The price is drastically effected by what your needs are. A business user looking to automate their data entry into Excel might be $100, while a complex, enterprise eCommerce mobile-ready website might be several thousand. Let's talk about what your specific needs are. Meetings are free!"
                     readMoreTitle="But what about IT support and 1099 indepent, enterprise contractors?"
-                    readMoreBody="I do not offer IT support as a service. If you have problems or questions with a product I provided to you, that's free of charge. If you're a recruiter or other person who is looking for a temporary hourly developer for their product team, please reach out for me for rate negotiation."
+                    readMoreBody="I do not offer IT support as a service. If you have problems or questions with a product I provided to you, that's free of charge. If you're a recruiter or other person who is looking for a temporary hourly developer for their product team, please reach out to me to talk about your project and negotiate a rate."
                 />
             </div>
 
