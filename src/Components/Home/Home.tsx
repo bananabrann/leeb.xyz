7beab18549c715f0def290ac92cbcbb452d8f243
bananabrann <plbrannan95@gmail.com>
2021-05-26 14:54:51 -0400

    Layout change

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -9,7 +9,7 @@ const Home: React.FC<any> = (props: any) => {
             document.getElementById("underscore");
 
         setInterval(function () {
-            // prettier-ignore 
+            // prettier-ignore
             underscore!.style.opacity = underscore!.style.opacity === "0" ? "1" : "0";
         }, blinkSpeed);
     }, []);
@@ -25,47 +25,45 @@ const Home: React.FC<any> = (props: any) => {
             <div className="content">
                 <h1>
                     lee<span id="underscore">_</span>b.
+                    {/* prettier-ignore */}
                     <span className="x" onMouseOver={(e) => handleMouseHoverEffect(e.target, "x")}>x</span>
+                    {/* prettier-ignore */}
                     <span className="y" onMouseOver={(e) => handleMouseHoverEffect(e.target, "y")}>y</span>
+                    {/* prettier-ignore */}
                     <span className="z" onMouseOver={(e) => handleMouseHoverEffect(e.target, "z")}>z</span>
                 </h1>
 
-                {/* 
-                    <h2>View my...</h2>
-                    <a href="./itil4foundation.pdf" target="_blank">
-                        <p>ITIL 4 Foundation in IT Service Management</p>
-                    </a>
-                    <a href="./msazuredevassociate.pdf" target="_blank">
-                        <p>Microsoft Certified Azure Developer Associate</p>
-                    </a>
-                    */}
-
-                <br />
-                <br />
-
-                <div className="logo-row">
-                    <a
-                        href="https://www.linkedin.com/in/pbrann/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <i className="fab fa-2x fa-linkedin"></i>
-                    </a>
-                    <a
-                        href="https://github.com/bananabrann"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <i className="fab fa-2x fa-github"></i>
-                    </a>
-                    <a
-                        href="https://stackoverflow.com/users/11057120/bananabrann"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <i className="fab fa-2x fa-stack-overflow"></i>
-                    </a>
+                <div className="section links">
+                    <div id="social-media">
+                        {/* prettier-ignore */}
+                        <a href="https://www.linkedin.com/in/pbrann/" target="_blank" rel="noreferrer">
+                            <i className="fab fa-2x fa-linkedin"></i>
+                        </a>
+                        {/* prettier-ignore */}
+                        <a href="https://github.com/bananabrann" target="_blank" rel="noreferrer">
+                            <i className="fab fa-2x fa-github"></i>
+                        </a>
+                        {/* prettier-ignore */}
+                        <a href="https://stackoverflow.com/users/11057120/bananabrann" target="_blank" rel="noreferrer">
+                            <i className="fab fa-2x fa-stack-overflow"></i>
+                        </a>
+                    </div>
+                    <line />
+                    <div id="professional-links">
+                        <a href="./resume.pdf" target="_blank">
+                            <p>Resume</p>
+                        </a>
+                        <a href="./itil4foundation.pdf" target="_blank">
+                            <p>ITIL 4 Foundation in IT Service Management</p>
+                        </a>
+                        <a href="./msazuredevassociate.pdf" target="_blank">
+                            <p>Microsoft Certified Azure Developer Associate</p>
+                        </a>
+                    </div>
                 </div>
+
+                <br />
+                <br />
             </div>
         </div>
     );
