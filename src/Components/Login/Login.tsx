51e74429070c9df1a1965e0649219e63a508c96c
bananabrann <plbrannan95@gmail.com>
2021-05-27 20:31:05 -0400

    Login transition

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Login.scss";
 
 const Login: React.FC<any> = (props: any) => {
-    return(
+    useEffect(() => {
+        const pamEle = document.getElementById("pam-logo");
+        const infoEle = document.getElementById("info-block");
+
+        pamEle!.classList.add("displayed");
+
+        setTimeout(() => {
+            pamEle!.classList.add("red");
+            pamEle!.children[1].classList.add("hide");
+
+            infoEle!.classList.remove("hide");
+            infoEle!.classList.add("displayed");
+        }, 2000);
+    }, []);
+
+    return (
         <div className="Login">
-            <div className="input-container">
-                <i className="fas fa-3x fa-lock"></i>
-                <input type="text" />
-                <input type="password" />
+            <div id="pam-logo">
+                <i className="fas fa-cubes fa-7x"></i>
+                <h2>P.A.M.</h2>
+            </div>
+
+            <div id="info-block" className="hide">
+                <div className="input-container">
+                    <input type="text" />
+                    <input type="password" />
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
